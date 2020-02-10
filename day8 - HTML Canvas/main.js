df4f7801ac96d6176c5f15800ee98b7f48e6f5e7
elenapayan <elena.payan.glez@gmail.com>
2020-02-10 19:53:09 +0100

    Improve style CSS for day8 project

--- day8 - HTML Canvas/main.js
+++ day8 - HTML Canvas/main.js
@@ -4,10 +4,12 @@ const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.heigth = window.innerHeight;
+canvas.setAttribute("width", "900");
+canvas.setAttribute("height", "500");
 ctx.strokeStyle = "#FABADA";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 100;
+ctx.lineWidth = 80;
 
 let isDrawing = false;
 let lastX = 0;
@@ -28,7 +30,7 @@ function draw(ev) {
   if (hue >= 360) {
     hue = 0;
   }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+  if (ctx.lineWidth >= 80 || ctx.lineWidth <= 1) {
     direction = !direction;
   }
   if (direction) {
